866b2d1210653297621aa2194e929ad59bb8b7b1
Carlos Delgadillo <carlos.delgadillo102003@gmail.com>
2024-07-18 01:24:15 -0600

    v1.7

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,14 @@
 # Dockerfile para Flask
 FROM python:3.11-slim
 
+#FROM ubuntu:16.04
+#RUN apt-get update && apt-get install -y --no-install-recommends \
+##    python3.5 \
+#    python3-pip \
+#    && \
+#apt-get clean && \
+#rm -rf /var/lib/apt/lists/*
+
 # Establecer el directorio de trabajo
 RUN mkdir /app
 WORKDIR /app
@@ -14,8 +22,9 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copiar el código de la aplicación Antes era web/ .
 COPY . .
 
+ENV PUERTO=5000
 
-EXPOSE 5000
+EXPOSE $PUERTO
 
 CMD ["python", "app.py"]
 
--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 #Pagina web
 from flask import Flask, render_template, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
+
+# Obtener las IPs de los microservicios desde las variables de entorno sin valores por defecto
+URL_SUMA = os.getenv('URL_SUMA')
+URL_RESTA = os.getenv('RESTAR_END')
+MULTIPLICAR_END = os.getenv('MULTIPLICAR_END')
+DIVIDIR_END = os.getenv('DIVIDIR_END')
+# Obtener el puerto desde la variable de entorno
+PUERTO = int(os.getenv('PUERTO'))
 
 @app.route('/')
 def index():
@@ -12,19 +21,23 @@ def index():
 def calcular():
     expression = request.form['expression']
 
+    # Verificar que todas las URLs necesarias estén definidas
+    #if not all([SUMAR_SERVICE_URL, RESTAR_SERVICE_URL, MULTIPLICAR_SERVICE_URL, DIVIDIR_SERVICE_URL]):
+        #return render_template('index.html', resultado='Error: Las variables de entorno de los microservicios no están configuradas')
+
     # Realiza la solicitud al microservicio correspondiente
     if '+' in expression:
         num1, num2 = map(float, expression.split('+'))
-        response = requests.post('http://192.168.49.1:8085/sumar', json={'num1': num1, 'num2': num2})
+        response = requests.post(URL_SUMA , json={'num1': num1, 'num2': num2})
     elif '-' in expression:
         num1, num2 = map(float, expression.split('-'))
-        response = requests.post('http://192.168.56.1:8004/restar', json={'num1': num1, 'num2': num2})
+        response = requests.post(URL_RESTA, json={'num1': num1, 'num2': num2})
     elif '*' in expression:
         num1, num2 = map(float, expression.split('*'))
-        response = requests.post('http://192.168.56.1:8003/multiplicar', json={'num1': num1, 'num2': num2})
+        response = requests.post(MULTIPLICAR_END, json={'num1': num1, 'num2': num2})
     elif '/' in expression:
         num1, num2 = map(float, expression.split('/'))
-        response = requests.post('http://192.168.56.1:8002/dividir', json={'num1': num1, 'num2': num2})
+        response = requests.post(DIVIDIR_END, json={'num1': num1, 'num2': num2})
     else:
         return render_template('index.html', resultado='Error: Operación no válida')
 
@@ -35,4 +48,4 @@ def calcular():
         return render_template('index.html', resultado='Error en la solicitud al microservicio')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=PUERTO) #host='0.0.0.0' hace que la aplicación esté accesible desde cualquier dirección IP de la máquina.
